b302b980c81b5e0c10cd46e0cabd3ab7fee423ab
Indiana Lora <indiana@ensaludybelleza.com>
2022-03-03 10:37:35 -0400

    create a wacth date-time section

--- script.js
+++ script.js
@@ -4,31 +4,46 @@ const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 const timer = document.getElementById("timer");
 
+const todoContainer = document.getElementById("todo-container");
+const input = document.getElementById("todoInput");
+const addButton = document.getElementById("addTodo")
+
 const clock = document.createElement("h1");
 const date = document.createElement("div");
 const dateTime = document.createElement("h1");
-const br = document.getElementById("BR");
+const localTime = document.createElement("h3");
+
+const todo=document.createElement("h3");
+
 clock.className = 'center';
 
 var interval;
 var time = 1;
+var todoArray =[];
 
-
-//setInterval(function(){
-currentDate();
-//},60* 1000)
+setInterval(function () {       
+    currentDate();
+},1000)
 
 function currentDate() {
     const today = new Date();
-    const currentDate  = new Date(Date.UTC(today.getFullYear(), today.getDate(), today.getMonth(), today.getHours() +" ", 0, 0));
+    const currentDate = new Date(Date.UTC(today.getFullYear(), today.getDate(), today.getMonth(), today.getHours() + " ", 0, 0));
     const renderDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
     dateTime.innerHTML = currentDate.toLocaleDateString(undefined, renderDate);
-
+    
+    var time = today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds();
+        localTime.innerHTML = time;
 }
- function currentTime(){
-    var time = today.getHours() + ":" + today.getMinutes();
- }
+
+function todoList(){
+   todoArray.push(input.value)
+   todoArray.forEach(element =>{
+       var list = document.createElement("li");
+       list.innerHTML=element;
+       todoContainer.appendChild(list);
+   })
+   input.value = " ";
+}
 
 function startTimer() {
     interval = setInterval(function () {
@@ -60,7 +75,6 @@ function pomodoroTime(time) {
         setInterval(function () {
             //add audio here
         }, 5000)
-
     }
 }
 
@@ -68,5 +82,9 @@ start.addEventListener("click", startTimer);
 stop.addEventListener("click", stopTimer);
 reset.addEventListener("click", resetTimer);
 
+addButton.addEventListener("click",todoList);
+
+
 timer.appendChild(clock);
 timezone.appendChild(dateTime);
+timezone.appendChild(localTime);
