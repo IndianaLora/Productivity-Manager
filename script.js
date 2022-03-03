88cc2762bd677812715724e34ae474ae799d8cc2
Indiana Lora <indiana@ensaludybelleza.com>
2022-03-03 10:44:09 -0400

    fix watch bug

--- script.js
+++ script.js
@@ -9,32 +9,32 @@ const input = document.getElementById("todoInput");
 const addButton = document.getElementById("addTodo")
 
 const clock = document.createElement("h1");
+clock.className = 'center';
 const date = document.createElement("div");
 const dateTime = document.createElement("h1");
 const localTime = document.createElement("h3");
-
 const todo=document.createElement("h3");
 
-clock.className = 'center';
 
 var interval;
 var time = 1;
 var todoArray =[];
 
-setInterval(function () {       
-    currentDate();
-},1000)
 
 function currentDate() {
-    const today = new Date();
+    var today = new Date();
     const currentDate = new Date(Date.UTC(today.getFullYear(), today.getDate(), today.getMonth(), today.getHours() + " ", 0, 0));
     const renderDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     dateTime.innerHTML = currentDate.toLocaleDateString(undefined, renderDate);
     
     var time = today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds();
-        localTime.innerHTML = time;
+    localTime.innerHTML = time;
 }
-
+function currentTime(){
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds();
+    localTime.innerHTML = time;
+}
 function todoList(){
    todoArray.push(input.value)
    todoArray.forEach(element =>{
@@ -88,3 +88,9 @@ addButton.addEventListener("click",todoList);
 timer.appendChild(clock);
 timezone.appendChild(dateTime);
 timezone.appendChild(localTime);
+
+
+setInterval(function () {       
+    currentTime();
+},1000)
+currentDate();
