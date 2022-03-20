802d0f87684d80f4ccbb300dc6bc12d48d9c556f
Indiana Lora <indiana@ensaludybelleza.com>
2022-03-19 20:09:41 -0400

    update manager

--- script.js
+++ script.js
@@ -22,130 +22,132 @@ var todoArray = [];
 var deleteHtlmItem;
 
 function currentDate() {
-    var today = new Date();
-    const currentDate = new Date(
-        Date.UTC(
-            today.getFullYear(),
-            today.getMonth(),
-            today.getDate(),
-            today.getHours() + " ",
-            0,
-            0
-        )
-    );
-    const renderDate = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    };
-    dateTime.innerHTML = currentDate.toLocaleDateString(undefined, renderDate);
+  var today = new Date();
+  const currentDate = new Date(
+    Date.UTC(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      today.getHours() + " ",
+      0,
+      0
+    )
+  );
+  const renderDate = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  dateTime.innerHTML = currentDate.toLocaleDateString(undefined, renderDate);
 
-    var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    localTime.innerHTML = time;
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  localTime.innerHTML = time;
 }
 function currentTime() {
-    var today = new Date();
-    var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    localTime.innerHTML = time;
+  var today = new Date();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  localTime.innerHTML = time;
 }
 function addTodo() {
-    const name = input.value;
-    if (name.trim() === "") {
-        return;
-    }
+  const name = input.value;
+  if (name.trim() === "") {
+    return;
+  }
 
-    const newId =
-        todoArray.length === 0 ? 1 : todoArray[todoArray.length - 1].id + 1;
-    todoArray.push({
+  var todos = JSON.parse(localStorage.getItem("todos"));
+  const newId =
+    todoArray.length === 0 ? 1 : todoArray[todoArray.length - 1].id + 1;
+  todos !== null
+    ? console.log("hey")
+    : todoArray.push({
         id: newId,
         name,
-    });
-    localStorage.setItem("todos",JSON.stringify(todoArray));
-    renderTodo(newId, name);
+      });
+      
+  input.value = "";
+  localStorage.setItem("todos", JSON.stringify(todoArray));
+  console.log(todos);
+  renderTodo();
 }
 
 function renderTodo() {
-    var todos=JSON.parse(localStorage.getItem("todos")) ;
-    todos.forEach(element => {
-        var listItem = document.createElement("li");
-        listItem.innerHTML = element.name;
-        listItem.id =element.id;
-        listItem.className="todo-element";
-    
-        var checkBox = document.createElement("input");
-        checkBox.setAttribute("type", "checkbox");
-        checkBox.classList="check";
-    
-        var deleteItem = document.createElement("i");
-        deleteItem.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-        deleteItem.style.cursor = "pointer";
-    
-        todoContainer.appendChild(listItem);
-        listItem.appendChild(checkBox);
-        listItem.appendChild(deleteItem);
-    
-        registerCheckEvent(checkBox);
-        registerDeleteEvent(deleteItem, listItem);
-    
-        input.value = "";
-    });
- 
+  var todos = JSON.parse(localStorage.getItem("todos"));
+  if (todos !== null) {
+    var element = todos[todos.length - 1];
+    var listItem = document.createElement("li");
+    listItem.innerHTML = element.name;
+    listItem.id = element.id;
+    listItem.className = "todo-element";
 
+    var checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.classList = "check";
 
+    var deleteItem = document.createElement("i");
+    deleteItem.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+    deleteItem.style.cursor = "pointer";
+
+    todoContainer.appendChild(listItem);
+    listItem.appendChild(checkBox);
+    listItem.appendChild(deleteItem);
+
+    registerCheckEvent(checkBox);
+    registerDeleteEvent(deleteItem, listItem);
+  }
 }
 
 function registerCheckEvent(checkBox) {
-    var itemChecked = checkBox.parentElement;
-    //If activity its been checked mark it as check
-    checkBox.addEventListener("click", function () {
-        if (checkBox.checked == true) {
-            itemChecked.style.textDecoration = "line-through";
-        } else {
-            itemChecked.style.textDecoration = "none";
-        }
-    });
+  var itemChecked = checkBox.parentElement;
+  //If activity its been checked mark it as check
+  checkBox.addEventListener("click", function () {
+    if (checkBox.checked == true) {
+      itemChecked.style.textDecoration = "line-through";
+    } else {
+      itemChecked.style.textDecoration = "none";
+    }
+  });
 }
 function registerDeleteEvent(itemToDelete, todo) {
-    itemToDelete.addEventListener("click", function () {
-        var index = todoArray.findIndex((item) => item.id === +todo.id);
-        todoArray.splice(index, 1);
-        deleteHtlmItem = document.getElementById(String(todo.id)).remove();
-    });
+  itemToDelete.addEventListener("click", function () {
+    var index = todoArray.findIndex((item) => item.id === +todo.id);
+    todoArray.splice(index, 1);
+    deleteHtlmItem = document.getElementById(String(todo.id)).remove();
+  });
 }
 function startTimer() {
-    interval = setInterval(function () {
-        var hours = Math.floor(time / 60);
-        var minutes = time % 60;
-        clock.innerHTML = hours + ":" + minutes;
-        time++;
-        pomodoroTime(time);
-    }, 1000);
-    start.className = "disabled";
+  interval = setInterval(function () {
+    var hours = Math.floor(time / 60);
+    var minutes = time % 60;
+    clock.innerHTML = hours + ":" + minutes;
+    time++;
+    pomodoroTime(time);
+  }, 1000);
+  start.className = "disabled";
 }
 
 function stopTimer() {
-    clearInterval(interval);
-    start.classList.remove("disabled");
-    start.className = "buttons";
+  clearInterval(interval);
+  start.classList.remove("disabled");
+  start.className = "buttons";
 }
 function resetTimer() {
-    clearInterval(interval);
-    time = 1;
-    clock.innerHTML = 0;
+  clearInterval(interval);
+  time = 1;
+  clock.innerHTML = 0;
 
-    start.classList.remove("disabled");
-    start.className = "buttons";
+  start.classList.remove("disabled");
+  start.className = "buttons";
 }
 function pomodoroTime(time) {
-    if (time === 1500) {
-        clearInterval(interval);
-        setInterval(function () {
-            //add audio here
-        }, 5000);
-    }
+  if (time === 1500) {
+    clearInterval(interval);
+    setInterval(function () {
+      //add audio here
+    }, 5000);
+  }
 }
 
 start.addEventListener("click", startTimer);
@@ -159,6 +161,6 @@ timezone.appendChild(dateTime);
 timezone.appendChild(localTime);
 
 setInterval(function () {
-    currentTime();
+  currentTime();
 }, 1000);
 currentDate();
